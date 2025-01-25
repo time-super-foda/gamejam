93a708488c6d39de6b13e5cf9f7dabe0e0d5bb79
julia-tatim <116753015+julia-tatim@users.noreply.github.com>
2025-01-25 11:51:25 -0300

    Player animação
    Adicionando a animação dos player

--- Player/player.gd
+++ Player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 300.0
@@ -9,17 +10,29 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if (velocity.x > 1 || velocity.x < -1):
+		animated_sprite_2d.animation = "walk"
+	else:
+		animated_sprite_2d.animation = "idle"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("player1_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite_2d.animation = "walk"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("player1_walk_left", "player1_walk_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.x< 0:
+			animated_sprite_2d.flip_h = false
+		if velocity.x>0:
+			animated_sprite_2d.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	var isLeft = velocity.x > 0
+	
