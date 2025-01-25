58124b7b74de9b74d70e9eb64503204f6eb1b76a
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2025-01-25 00:48:04 -0300

    player v0.1

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 450.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -9,17 +9,22 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		$AnimatedSprite2D.play("jump")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
+	# Player jump.
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction := Input.get_axis("player_walk_left", "player_walk_right")
+
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
 
 	move_and_slide()
