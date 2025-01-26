e64e31a7d6fbf147d4d1cc6ff100bea77f31eede
enzo enrico <e.enrico2005@gmail.com>
2025-01-26 12:30:52 -0300

    splash

--- gui.gd
+++ gui.gd
@@ -12,6 +12,6 @@ func _on_play_pressed() -> void:
 
 func _on_leaderboard_pressed() -> void:
 	get_tree().change_scene_to_file("res://creditScene.tscn")
-	
+
 func _on_back_principal_scene_pressed() -> void:
-	get_tree().change_scene_to_file("res://start_screen.tscn")
+	get_tree().change_scene_to_file("res://new_splash.tscn")
